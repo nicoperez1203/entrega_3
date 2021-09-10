59af6562e3702ffca5b6c39bd038b9829b36a5d8
Nicolas Perez <nipefo1203@gmail.com>
2021-09-10 10:44:52 -0300

    entrega_3.1

--- js/product-info.js
+++ js/product-info.js
@@ -31,6 +31,7 @@ function mostrarInfoAuto(autos) {
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+
         if (resultObj.status === "ok") {
             let veichulo = resultObj.data;
 
@@ -38,19 +39,66 @@ document.addEventListener("DOMContentLoaded", function (e) {
             let descAuto = document.getElementById("descAuto");
             let vendAuto = document.getElementById("vendAuto");
             let costAuto = document.getElementById("costAuto");
+            let cateAuto = document.getElementById("cateAuto");
+
 
             nombAuto.innerHTML = veichulo.name;
             descAuto.innerHTML = veichulo.description;
             vendAuto.innerHTML = veichulo.soldCount;
             costAuto.innerHTML = veichulo.currency + veichulo.cost;
+            cateAuto.innerHTML = veichulo.category;
+
 
 
             mostrarInfoAuto(veichulo.images);
         }
+
+
+
     });
 });
 
-var comentarios = {};
+
+
+var prodRel = [];
+function mostrarProdRel(prodRel){
+    
+    let listRelacionados = "";
+    for (let i = 0; i < prodRel.length; i++){
+        let rePro = prodRel[i];
+        {
+            listRelacionados += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + rePro.imgSrc + `" alt="Onix">
+            </div>
+        </div>
+        `
+        }
+
+        document.getElementById("productosRelacionados").innerHTML = listRelacionados;
+    }
+
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+
+        if (resultObj.status === "ok") {
+            
+
+            let relatedProducts = resultObj.data;
+            mostrarProdRel(relatedProducts);
+
+        }
+    });
+});
+
+
+
+
+
+
+var comentarios = [];
 
 function mostrarComentarios(comentarios) {
 
@@ -60,21 +108,22 @@ function mostrarComentarios(comentarios) {
 
         {
             listaComentarios += `
-                
-                    <div class="row">
-                    <h4 class="mb-1">`+ coment.user + `</h4> 
+                    
+                   
+                    <div>
+                    <h5><img src="img/user.png" width="45" height="25"><font color="Olive"><strong>`+ coment.user + `</font></strong></img></h5> 
                     <br>
-                        <div class="col-3">
-                            <p> `+ coment.description + ` </p>
+                        <div>
+                        <i> "`+ coment.description + `" </i>
                         </div>
-                        <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                
-                                <small class="text-muted">Fecha y hora del comentario: ` + coment.dateTime + ` </small>
-                            </div>
-                            <p class="mb-1">` + coment.score + `</p>
+                        <div>
+                            <p>` + coment.score + ` Estrellas</p>
+                             <div>
+                              <small class="text-muted">Fecha y hora del comentario: ` + coment.dateTime + ` </small>
+                             </div>
                         </div>
                     </div>
+                    <hr>
                 
                 `
         }
