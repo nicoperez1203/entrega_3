6a29d0a4719df70a54ecd483113d0af7396583c9
Nicolas Perez <nipefo1203@gmail.com>
2021-09-17 22:21:55 -0300

    entrega_3_terminada_ahorasi

--- js/enviarComentario.js
+++ js/enviarComentario.js
@@ -2,6 +2,7 @@
 //DESAFIATE ENTREGA 3
 
 
+
 function enviarComentario(){
     var description = document.getElementById("newComentario").value;
     var hoy = new Date();
@@ -9,9 +10,49 @@ function enviarComentario(){
     var hora = hoy.getHours() + `:` + hoy.getMinutes() + `:` + hoy.getSeconds();
     var user = JSON.parse(sessionStorage.getItem("user"));
     
-    
+    var stars = document.getElementById("puntuacion").value;
+         if (stars == 1) {
+            estrellas = `<span class="fa fa-star checked"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>`
+            } else if (stars == 2) {
+            estrellas = `<span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>`
+
+            }
+            else if (stars == 3) {
+            estrellas = `<span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star"></span>
+                         <span class="fa fa-star"></span>`
+
+            } else if (stars == 4) {
+            estrellas = `<span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star"></span>`
+
+            } else if (stars == 5) {
+            estrellas = `<span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>
+                         <span class="fa fa-star checked"></span>`
+
+                 }
+   
+   
+
     var insertComment =`<div><h5><img src="img/user.png" width="45" height="25"><font color="Olive"><strong> ` + user.usuario + `</font></strong></img></h5></div>
-                        <br>  <div>"<i>` + description + `</i>"</div> <br> <small class="text-muted">Fecha y hora del comentario:` + fecha + ` ` + hora + `</small><hr>`;
+                        <br>  <div>"<i>` + description + `</i>"</div><br> Puntuación:`
+                        + estrellas +`<br><small class="text-muted">Fecha y hora del comentario:` + fecha + ` ` + hora + `</small><hr>`;
     
     
     if (document.getElementById("newComentario").value == ""){
